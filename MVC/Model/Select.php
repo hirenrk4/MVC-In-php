742366e0a8850b31fac609af0f91ee64113f9a17
Hiren Kadivar <hirenkadivar13@gmail.com>
2016-11-04 17:24:33 +0530

    Create Select.php

---
+++ MVC/Model/Select.php
@@ -0,0 +1,17 @@
+<?php  
+   class Select extends Model  
+   {  
+      function __construct()  
+      {  
+         // Call the Model constructor  
+         parent::__construct();  
+      }  
+      //we will use the select function  
+      public function select()  
+      {  
+         //data is retrive from this query  
+         $query = $this->db->get('country');  
+         return $query;  
+      }  
+   }  
+?> 
